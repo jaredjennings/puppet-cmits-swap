40c2869c5b0ec9fdbfa6e5e5e16bba812a9ca0c4
Jared Jennings <jjennings@fastmail.fm>
2015-08-10 17:06:54 -0500

    take care of case and quoting in osfamily selectors; get rid of unimplemented() uses (Puppet 4 compatibility)

--- manifests/encrypt/redhat.pp
+++ manifests/encrypt/redhat.pp
@@ -14,5 +14,5 @@
 # % See the License for the specific language governing permissions and
 # % limitations under the License.
 class swap::encrypt::redhat {
-    unimplemented()
+    fail "unimplemented"
 }
